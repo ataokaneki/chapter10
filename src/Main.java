08a90aa290ddfa381023999afeb5666f0c1b9466
ataokaneki <241069@std.hi-joho.ac.jp>
2025-06-13 12:18:04 +0900

    10

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,6 @@
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        SuperHero sh = new SuperHero();
-        /*sh.fly();
-        sh.run();
-        sh.test();
-        Hero h = new Hero();
-        h.run();
-        h.test();
-        */
+        Weapon w = new Weapon();
     }
 }
